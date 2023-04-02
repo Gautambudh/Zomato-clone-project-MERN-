89f775e7afd4dcc2ba6ab533d2bddb3659eb1ad1
Gautam <gautamk2ch@gmail.com>
2023-04-02 11:13:55 +0530

    fixing the database connection for fast response

--- server/index.js
+++ server/index.js
@@ -11,19 +11,40 @@ const mongodb_uri = `${process.env.mongodb_uri}?retryWrites=true&w=majority`
 app.use(cors()); // to enable the CORS policy
 app.use(express.json()); // string data to json 
 app.use(express.urlencoded({extended: false})); // if extended!=false, it will give get, put & post also in body
-// adding external routing
-// middleware
-app.use('/', APIRouter)
+
+
 
 // before starting the connection we had to check the db connection
-mongoose.connect(mongodb_uri)
-.then( () => {
-    console.log("DB connected successfuly")
-    app.listen(PORT, () => {
-        console.log("Server Running on port", PORT);
-    })
-})
-.catch((e) => {
-    console.log("error while connecting to db");
-    console.log(e);
-})
+// mongoose.connect(mongodb_uri)
+// .then( () => {
+//     console.log("DB connected successfuly")
+//     app.listen(PORT, () => {
+//         console.log("Server Running on port", PORT);
+//     })
+// })
+// .catch((e) => {
+//     console.log("error while connecting to db");
+//     console.log(e);
+// })
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(mongodb_uri);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log("error while connecting to db");
+      console.log(error);
+    }
+  }
+  
+  // adding external routing
+// middleware
+  //Routes go here
+  app.use('/', APIRouter)
+  
+  //Connect to the database before listening
+  connectDB().then(() => {
+      app.listen(PORT, () => {
+          console.log("listening for requests on port", PORT);
+      })
+  })
