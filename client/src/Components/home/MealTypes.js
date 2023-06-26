541ad61cca0a3890b5fd748a5007aab62bf3fc5b
Gautam <gautamk2ch@gmail.com>
2023-06-26 23:46:06 +0530

    adding loading skeleton for cards while data is being fetched from the database

--- client/src/Components/home/MealTypes.js
+++ client/src/Components/home/MealTypes.js
@@ -1,25 +1,32 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { Box, Skeleton } from '@mui/material';
 import axios from "axios";
 
 function MealTypes() {
     // instance of navigate
     let navigate = useNavigate();
     let [mealList, setMealList] = useState([]); // #1
-    
+    const [loading, setLoading] = useState(true);
 
-    let getMenuListFromServer = async () => {
-        let url = "https://developer-gautam-zomato-clone.onrender.com/api/get-Mealtype-list";
-        let { data } = await axios.get(url); //#4
-        // console.log(data.Meal_types);
-        setMealList([...data.Meal_types])  // #5 used spread operator to recreate the memory 
-    }
+    const getMenuListFromServer = async () => {
+        try {
+          const url = "https://developer-gautam-zomato-clone.onrender.com/api/get-Mealtype-list";
+          const { data } = await axios.get(url); //#4
+          setMealList([...data.Meal_types]); // #5 used spread operator to recreate the memory
+          setLoading(false); // once the data is loaded in array, loading will be false and cards will render
+        } catch (error) {
+          console.error("Error fetching meal types:", error);
+          setLoading(false); // Set loading to false in case of error as well
+        }
+      };
 
-    // console.log(mealList);
+    console.log(mealList);
     useEffect(() => {
         getMenuListFromServer(); // #3
-    }, []); // when we use empty array in useEffect() , it'll only run once on page load
-            // useEffect runs when there is change in state in component.
+    }, []);  // when we use empty array in useEffect() , it'll only run once on page load
+             // useEffect runs when there is change in state in component.
+    
     return (
     <>
         {/* #2 */}
@@ -32,10 +39,36 @@ function MealTypes() {
         </div>
         {/* all the six cards */}
         <div className="row row-cols-lg-3 row-cols-md-3 row-cols-sm-1 row-cols-1 all-cards">
-            {
-            mealList.map((meal, index) => {
+            {loading ? 
+                (
+                // Showing skeleton or loading indicator while data is being fetched
+                [1,2,3,4,5,6].map((val, ind) => {
+                    return (
+                    <div key={ind} className="col-4 cards">
+                        <div className=" col-12 ">
+                        <div className="row g-0">
+                            <div className="col-4">
+                            <Skeleton variant="rectangular" width={120} height={160} />
+                            </div>
+                            <div className=" col-8">
+                            <div className="card-body">
+                            <Skeleton variant="text" className="card-title" width={70} sx={{ fontSize: '1.5rem' }} />
+                            <Skeleton variant="text" className="card-text" sx={{ fontSize: '1rem' }} />
+                            <Skeleton variant="text" className="card-text" sx={{ fontSize: '1rem' }} />
+                            <Skeleton variant="text" className="card-text" sx={{ fontSize: '1rem' }} />
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                )
+                })
+                ) 
+                :
+                (mealList.map((meal, index) => {
                 return (
-                <div key={meal._id} className="col-4 cards" onClick={()=> navigate("/quick-search/"+meal.meal_type)}>
+                    <>
+                    <div key={meal._id} className="col-4 cards" onClick={()=> navigate("/quick-search/"+meal.meal_type)}>
                     <div className=" col-12 ">
                         <div className="row g-0">
                             <div className="col-4">
@@ -49,11 +82,11 @@ function MealTypes() {
                             </div>
                         </div>
                     </div>
-                </div>
-                    )
-                })
+                    </div>
+                    </>
+                )}
+                ))
             }
-            
         </div>
     </div>
     </>
